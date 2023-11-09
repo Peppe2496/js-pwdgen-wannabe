a5ec0e66413d75359a73f951f84cf762af71b77e
Peppe2496 <giuseppemusellajob@gmail.com>
2023-11-09 14:37:06 +0100

    finished base exercise

--- js/main.js
+++ js/main.js
@@ -1,9 +1,14 @@
 // Questo è il file che contiene tutto il codice JS della pagina web
 
-document.writeln("<h3>Ciao Mondo!</h3>");
+// Chiedere le informazioni di base all'utente
+let name = prompt("Inserisci il tuo nome");
+let surname = prompt("Inserisci il tuo cognome");
+let favoriteColor = prompt("Inserisci il tuo colore preferito");
+let number = 21;
 
-alert("Hello Word!");
+let passwordGenerator = `
 
-document.getElementById("title_page").innerHTML = "Ciao Mondo!";
+----Password: ${name}${surname}${favoriteColor}21
 
-console.log("Ciao Mondo!");
+`;
+console.log(passwordGenerator)
