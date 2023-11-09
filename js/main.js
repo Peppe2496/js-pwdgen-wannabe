de5c5e4adb99c68ebe8794eaf803359c483450ca
Peppe2496 <giuseppemusellajob@gmail.com>
2023-11-09 14:58:00 +0100

    finished the 3 type of password

--- js/main.js
+++ js/main.js
@@ -1,10 +1,12 @@
 // Questo è il file che contiene tutto il codice JS della pagina web
 
 // Chiedere le informazioni di base all'utente
+
+
 let name = prompt("Inserisci il tuo nome");
 let surname = prompt("Inserisci il tuo cognome");
-let favoriteColor = prompt("Inserisci il tuo colore preferito");
-let number = 21;
+let favoriteColor = prompt("Inserisci il tuo colore preferito");            // Esercizio base
+
 
 let passwordGenerator = `
 
@@ -12,3 +14,40 @@ let passwordGenerator = `
 
 `;
 console.log(passwordGenerator)
+
+
+/*
+let name = prompt("Inserisci il tuo nome");
+let surname = prompt("Inserisci il tuo cognome");
+let favoriteColor = prompt("Inserisci il tuo colore preferito");            // Password con divisione
+let firstNumber = prompt("Scegli un numero");
+let secondNumber = prompt("Scegli un secondo numero");
+
+// Risultato del calcolo
+
+firstNumber= parseInt(firstNumber);
+secondNumber= parseInt(secondNumber);
+let divisionResult = firstNumber / secondNumber;
+
+let passwordGenerator = `
+
+----Password: ${name}${surname}${favoriteColor}${divisionResult}
+
+`;
+console.log(passwordGenerator)
+*/
+
+/*
+let name = prompt("Inserisci il tuo nome");
+let surname = prompt("Inserisci il tuo cognome");
+let favoriteColor = prompt("Inserisci il tuo colore preferito");            // Password con numero random
+let randomNumber = Math.floor((Math.random() * 100) + 0);
+
+
+let passwordGenerator = `
+
+----Password: ${name}${surname}${favoriteColor}${randomNumber}
+
+`;
+console.log(passwordGenerator)
+*/
